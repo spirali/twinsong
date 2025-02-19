ede43ea45e0a00c84af99e2646d7378a568a7c3c
Ada Bohm <ada@kreatrix.org>
2025-02-19 12:51:20 +0100

    Up/Down movement in cells

--- browser/ui/src/components/EditorPanel.tsx
+++ browser/ui/src/components/EditorPanel.tsx
@@ -4,14 +4,29 @@ import { useGlobalState, useDispatch } from './StateProvider';
 import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-python';
-import 'prismjs/themes/prism.css'; //Example style, you can use another
+import 'prismjs/themes/prism.css';
 import { useSendCommand } from './WsProvider';
 import { run_cell } from '../core/actions';
 import { SquarePlus } from 'lucide-react';
 
+
+function checkIfLastLine(event: React.KeyboardEvent<HTMLDivElement> | React.KeyboardEvent<HTMLTextAreaElement>) {
+  const textarea = event.target as HTMLTextAreaElement;
+  const cursorPosition = textarea.selectionStart;
+  return !textarea.value.substring(cursorPosition).includes("\n");
+}
+
+function checkIfFirstLine(event: React.KeyboardEvent<HTMLDivElement> | React.KeyboardEvent<HTMLTextAreaElement>) {
+  const textarea = event.target as HTMLTextAreaElement;
+  const cursorPosition = textarea.selectionStart;
+  return !textarea.value.substring(0, cursorPosition).includes("\n");
+}
+
 const EditorCellRenderer: React.FC<{
   cell: EditorCell;
-}> = ({ cell }) => {
+  prev_id: string | null;
+  next_id: string | null;
+}> = ({ cell, prev_id, next_id }) => {
   const dispatch = useDispatch()!;
   const sendCommand = useSendCommand()!;
   const state = useGlobalState();
@@ -19,6 +34,7 @@ const EditorCellRenderer: React.FC<{
   return (
     <div className="mb-2 border border-gray-200 rounded-md overflow-hidden">
       <Editor
+        id={cell.id}
         value={cell.value}
         onValueChange={code => { dispatch({ type: "cell_edit", id: cell.id, value: code }) }}
         highlight={code => highlight(code, languages.python)}
@@ -31,9 +47,24 @@ const EditorCellRenderer: React.FC<{
           if (e.ctrlKey && e.key === 'Enter') {
             e.preventDefault();
             run_cell(cell, state, dispatch, sendCommand);
-          } 
-        }
-      }
+          }
+          if (e.key == "ArrowUp" && prev_id && checkIfFirstLine(e)) {
+            e.preventDefault();
+            let textArea = document.getElementById(prev_id)?.getElementsByTagName("textarea")[0];
+            if (textArea) {
+              textArea.focus();
+              textArea.setSelectionRange(textArea.value.length, textArea.value.length);
+            }
+          }
+          if (e.key == "ArrowDown" && next_id && checkIfLastLine(e)) {
+            e.preventDefault();
+            let textArea = document.getElementById(next_id)?.getElementsByTagName("textarea")[0];
+            if (textArea) {
+              textArea.focus();
+              textArea.setSelectionRange(0, 0);
+            }
+          }
+        }}
       />
     </div>
   );
@@ -60,10 +91,12 @@ const EditorPanel: React.FC = () => {
 
       {/* Cells Container */}
       <div className="p-4 space-y-4 overflow-auto">
-        {state.notebook!.editor_cells.map((cell) => (
+        {state.notebook!.editor_cells.map((cell, index) => (
           <EditorCellRenderer
             key={cell.id}
             cell={cell}
+            prev_id={state.notebook!.editor_cells[index - 1]?.id || null}
+            next_id={state.notebook!.editor_cells[index + 1]?.id || null}
           />
         ))}
       </div>
