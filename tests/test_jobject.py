26579b18563da9aad1235cb81d2d803dd0669a44
Ada Bohm <ada@kreatrix.org>
2025-03-05 21:08:42 +0100

    Tests fixed

--- tests/test_jobject.py
+++ tests/test_jobject.py
@@ -22,7 +22,7 @@ class FooBar:
     pass
 
 
-def test_jobject():
+def test_jobject_basic():
     assert build_obj(None) == {"kind": "null", "repr": "None"}
     assert build_obj(-123) == {"kind": "number", "repr": "-123", "value_type": "int"}
     assert build_obj(5) == {"kind": "number", "repr": "5", "value_type": "int"}
@@ -33,6 +33,8 @@ def test_jobject():
         "value_type": "float",
     }
 
+
+def test_jobject_list():
     assert build_obj([1, 2, 3]) == {
         "repr": "[1, 2, 3]",
         "value_type": "list[int]",
@@ -43,23 +45,40 @@ def test_jobject():
             ("2", {"repr": "3", "value_type": "int", "kind": "number"}),
         ],
     }
-
-    assert build_obj({"a": 1, "b": 2, "c": 9}) == {
-        "repr": "{'a': 1, 'b': 2, 'c': 9}",
-        "value_type": "dict[str, int]",
-        "kind": "dict",
+    assert build_obj([1, "a", 3]) == {
+        "repr": "[1, 'a', 3]",
+        "value_type": "list",
+        "kind": "list",
         "children": [
-            ("a", {"repr": "1", "value_type": "int", "kind": "number"}),
-            ("b", {"repr": "2", "value_type": "int", "kind": "number"}),
-            ("c", {"repr": "9", "value_type": "int", "kind": "number"}),
+            ("0", {"repr": "1", "value_type": "int", "kind": "number"}),
+            ("1", {"repr": "\"a\"", "value_type": "str", "kind": "string"}),
+            ("2", {"repr": "3", "value_type": "int", "kind": "number"}),
+        ],
+    }
+    assert build_obj(["f"] * 30) == {
+        "repr": "30 items",
+        "value_type": "list[str]",
+        "kind": "list",
+        "children": [
+            (str(i), {"repr": "\"f\"", "value_type": "str", "kind": "string"}) for i in range(30)
         ],
     }
 
-    r = build_obj(FooBar())
-    assert "FooBar" in r["repr"]
-    del r["repr"]
-    assert r == {"value_type": "FooBar"}
 
+def test_jobject_tuple():
+    assert build_obj((1, 2, 3)) == {
+        "repr": "(1, 2, 3)",
+        "value_type": "tuple[int]",
+        "kind": "list",
+        "children": [
+            ("0", {"repr": "1", "value_type": "int", "kind": "number"}),
+            ("1", {"repr": "2", "value_type": "int", "kind": "number"}),
+            ("2", {"repr": "3", "value_type": "int", "kind": "number"}),
+        ],
+    }
+
+
+def test_jobject_recursive():
     x = []
     x.append(x)
     r = make_jobject(x)
@@ -71,8 +90,47 @@ def test_jobject():
                 "children": [["0", root]],
                 "id": root,
                 "kind": "list",
-                "repr": "1 items",
+                "repr": "[[...]]",
                 "value_type": "list",
             }
         ],
     }
+
+
+def test_jobject_dict():
+    assert build_obj({"a": 1, "b": 2, "c": 9}) == {
+        "repr": "{'a': 1, 'b': 2, 'c': 9}",
+        "value_type": "dict[str, int]",
+        "kind": "dict",
+        "children": [
+            ("a", {"repr": "1", "value_type": "int", "kind": "number"}),
+            ("b", {"repr": "2", "value_type": "int", "kind": "number"}),
+            ("c", {"repr": "9", "value_type": "int", "kind": "number"}),
+        ],
+    }
+    assert build_obj({1: "a", 2: "b", 9: "c"}) == {
+        "repr": "{1: 'a', 2: 'b', 9: 'c'}",
+        "value_type": "dict[int, str]",
+        "kind": "dict",
+        "children": [
+            ("1", {"repr": "\"a\"", "value_type": "str", "kind": "string"}),
+            ("2", {"repr": "\"b\"", "value_type": "str", "kind": "string"}),
+            ("9", {"repr": "\"c\"", "value_type": "str", "kind": "string"}),
+        ],
+    }
+    assert build_obj({i: f"f{i}" for i in range(15)}) == {
+        "repr": "15 items",
+        "value_type": "dict[int, str]",
+        "kind": "dict",
+        "children": [
+            (str(i), {"repr": f"\"f{i}\"", "value_type": "str", "kind": "string"})
+            for i in range(15)
+        ],
+    }
+
+
+def test_jobject_opaque():
+    r = build_obj(FooBar())
+    assert "FooBar" in r["repr"]
+    del r["repr"]
+    assert r == {"value_type": "FooBar"}
